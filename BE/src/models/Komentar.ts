bf30b74e85bb296643599b3222c85399072b8358
DionWsdm <dionwisdom29@gmail.com>
2025-03-27 23:30:44 +0700

    Models

---
+++ BE/src/models/Komentar.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+
+const createKomentar = (req: express.Request, res: express.Response) =>
+{
+
+}
+
+const getKomentar = (req: express.Request, res: express.Response) => 
+{
+
+}
+
+const updateKomentar = (req: express.Request, res: express.Response) =>
+{
+
+}
+
+const deleteKomentar = (req: express.Request, res: express.Response) =>
+{
+
+}
+
+export default {
+    createKomentar,
+    getKomentar,
+    updateKomentar,
+    deleteKomentar,
+}
