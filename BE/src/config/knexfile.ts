ed17737f18be1540b09cc2abee457a8f693dfe41
DionWsdm <dionwisdom29@gmail.com>
2025-03-27 23:30:08 +0700

    env config

---
+++ BE/src/config/knexfile.ts
@@ -0,0 +1,25 @@
+import type { Knex } from "knex";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: { [Key: string]: Knex.Config }  = {
+    development: {
+        client: "pg",
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: Number(process.env.DB_PORT),
+        },
+        migrations: {
+            directory: "./migrations",
+        },
+        seeds: {
+            directory: "./seeds",
+        }
+    },
+} 
+
+export default config;
