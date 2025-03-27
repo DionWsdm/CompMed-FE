ab672d54c6a684e408a0b7e40b1de68855cebe64
DionWsdm <dionwisdom29@gmail.com>
2025-03-27 23:31:04 +0700

    types

---
+++ BE/src/data/Postingan.ts
@@ -0,0 +1,10 @@
+type Post = 
+{
+    userid: number,
+    konten: string,
+    image_url: string,
+    created_at: Date,
+    username: string,
+}
+
+export default Post;
---
+++ BE/src/data/User.ts
@@ -0,0 +1,11 @@
+type User = {
+    id: number,
+    username: string,
+    email: string,
+    full_name: string,
+    pwd: string,
+    bio: string,
+    profile_picture: string,
+}
+
+export default User;
