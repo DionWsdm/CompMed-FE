53236cf82228d5c16e1feed9ad38126771decd77
DionWsdm <dionwisdom29@gmail.com>
2025-03-30 22:20:20 +0700

    debug

--- FE/src/pages/SignIn.tsx
+++ FE/src/pages/SignIn.tsx
@@ -7,7 +7,7 @@ const SignIn = async (valid: number, setValid: Function) =>
     console.log(Boolean(""))
     const username = (document.getElementById("username") as HTMLInputElement).value;
     const password = (document.getElementById("password") as HTMLInputElement).value;
-    const response = await fetch(`${import.meta.env.VITE_BE_URL}/login`, {
+    const response = await fetch(`https://comp-med-be-dion-wisdom-ps-projects.vercel.app/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({username: username, pwd: password}),
