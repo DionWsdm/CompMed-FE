035e881c8d7d0cee7ebeb3a83d538e5c33b0e5bc
DionWsdm <dionwisdom29@gmail.com>
2025-04-01 19:06:12 +0700

    Komponen daftar komen untuk suatu post

--- FE/src/components/Comments.tsx
+++ FE/src/components/Comments.tsx
@@ -1,6 +1,7 @@
 import Comment from "../@Types/Comment";
 import { useEffect, useState } from "react";
 import profilepic from "../assets/profilepic.png";
+import Utils from "../utils/Utils";
 
 const Comments = ({postid} : {postid: number}) =>
 {
@@ -14,15 +15,21 @@ const Comments = ({postid} : {postid: number}) =>
         .then((data) => setComments(data.comments))
         .catch((error) => console.log("Error saat mengambil data: ", error))
     }, [])
+    
     return (
         <>
             <div className="flex flex-col">
-                <p className="font-bold">Comments</p>
+                <p className="font-bold mb-2">Comments</p>
             {comments.map((comment) => (
                 <div className="flex flex-row gap-3">
-                    <img src={profilepic} alt="" />
-                    <div className="grid grid-cols-[20%_auto_auto]">
-                        <p>{comment.username}</p>
+                    <img src={profilepic} alt="" className="size-15"/>
+                    <div className="grid grid-rows-[20%_auto_auto] mt-1 gap-2">
+                        <div className="flex justify-items-start gap-1.5 text-gray-500 text-[0.9rem]" >
+                            <p className="text-[0.9rem]">{comment.username}</p> <p>·</p> <p className="text-[0.9rem]">{Utils.getDateString(new Date(comment.created_at))}</p>
+                        </div>
+                        <div>
+                            <p>{comment.content}</p>
+                        </div>
                     </div>
                 </div>
             ))}
