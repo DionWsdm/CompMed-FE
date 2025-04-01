fd314df39230e0d55846d5c8c5aefce3f1597394
DionWsdm <dionwisdom29@gmail.com>
2025-04-01 10:08:41 +0700

    Menambahkan rute untuk post

--- FE/src/routes/Router.tsx
+++ FE/src/routes/Router.tsx
@@ -3,6 +3,7 @@ import App from "../App";
 import SignInPage from "../pages/SignIn";
 import SignUpPage from "../pages/SignUp";
 import HomePage from "../pages/Home";
+import PostPage from "../pages/PostPage";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
         children: [
             {path: "/Home/", element: <HomePage />},
             {path: "/SignUp/", element: <SignUpPage />},
+            {path: "/Post/:username/:postid", element: <PostPage />},
             {path: "/", element: <SignInPage />},
         ]
     }
